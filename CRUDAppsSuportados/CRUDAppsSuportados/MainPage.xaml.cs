09be49068db51d31d0c998ac0d1062274362b1f6
Paulo Sergio <pscosta2008@gmail.com>
2025-06-22 15:24:35 -0300

    feat: adding request to get all apps

--- CRUDAppsSuportados/CRUDAppsSuportados/MainPage.xaml.cs
+++ CRUDAppsSuportados/CRUDAppsSuportados/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using CRUDAppsSuportados.Models;
 using CRUDAppsSuportados.Services;
+using CRUDAppsSuportados.ViewModels;
+using Microsoft.Maui;
 
 namespace CRUDAppsSuportados
 {
@@ -10,6 +12,7 @@ namespace CRUDAppsSuportados
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new AppSuportadoViewModel();
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -19,10 +22,11 @@ namespace CRUDAppsSuportados
             model.Descricao = DescEntry.Text;
             model.Referencia = RefEntry.Text;
             model.Situacao = SitEntry.Text;
-            model.Id = 0;  
+            model.Id = 0;
             string results = await service.PostApp(model);
 
-            await DisplayAlert("Results", ""+results, "OK");
+            BindingContext = new AppSuportadoViewModel();
+            await DisplayAlert("Results", "" + results, "OK");
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
---
+++ CRUDAppsSuportados/CRUDAppsSuportados/ViewModels/AppSuportadoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CRUDAppsSuportados.Models;
+using CRUDAppsSuportados.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDAppsSuportados.ViewModels
+{
+    class AppSuportadoViewModel
+    {
+
+        private readonly AppSuportadoService _appSuportadoService;
+        public ObservableCollection<AppSuportadoModel> Apps { get; set; } = new ObservableCollection<AppSuportadoModel>();
+
+
+        public AppSuportadoViewModel()
+        {
+            _appSuportadoService = new AppSuportadoService();
+            showApps();
+        }
+
+        public async void showApps()
+        {
+            List<AppSuportadoModel> apps = await _appSuportadoService.GetAll();
+            Apps.Clear();
+            foreach (var app in apps)
+            {
+                Apps.Add(app);
+            }
+        }
+
+
+    }
+}
